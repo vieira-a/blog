ae535d3c31ab7c6b70e74565c8d028c50783b109
Anderson Vieira <asvieira.br@gmail.com>
2023-04-19 20:04:02 -0300

    feat: render post content by id

--- src/components/Post/PostContent/index.tsx
+++ src/components/Post/PostContent/index.tsx
@@ -1,12 +1,31 @@
+import { useParams } from "react-router-dom";
+import postData from "../../../data/posts.json";
+import { IPosts } from "src/common/interfaces/IPosts";
+
+interface PostParamsProps {
+  postId: number;
+}
+
 export default function PostContent() {
+  const data = postData as IPosts;
+  const { id } = useParams();
+  const postId = Number(id);
+
   return (
     <section>
-      <div>
-        <p>Data</p>
-        <h2>Titulo</h2>
-        <p>Resumo</p>
-      </div>
-      <div>Conteudo</div>
+      {data.posts.map(
+        (post) =>
+          post.id === postId && (
+            <div key={post.id}>
+              <p>{post.date}</p>
+              <h2>{post.title}</h2>
+              <div>
+                <h4>{post.sumary}</h4>
+                <p>{post.content}</p>
+              </div>
+            </div>
+          )
+      )}
     </section>
   );
 }
