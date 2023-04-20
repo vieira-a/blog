9ff325ba8ce83d81cbcbf806fefccc25d36907ca
Anderson Vieira <asvieira.br@gmail.com>
2023-04-19 21:14:53 -0300

    feat: add navbar to post content

--- src/components/Post/PostContent/index.tsx
+++ src/components/Post/PostContent/index.tsx
@@ -1,31 +1,38 @@
 import { useParams } from "react-router-dom";
 import postData from "../../../data/posts.json";
 import { IPosts } from "src/common/interfaces/IPosts";
-
-interface PostParamsProps {
-  postId: number;
-}
+import Navbar from "../../Navbar";
+import { useState } from "react";
 
 export default function PostContent() {
   const data = postData as IPosts;
   const { id } = useParams();
   const postId = Number(id);
 
+  const [darkMode, setDarkMode] = useState<boolean>(true);
+
+  function toggleDarkMode() {
+    setDarkMode((prevDarkMode) => !prevDarkMode);
+  }
+
   return (
-    <section>
-      {data.posts.map(
-        (post) =>
-          post.id === postId && (
-            <div key={post.id}>
-              <p>{post.date}</p>
-              <h2>{post.title}</h2>
-              <div>
-                <h4>{post.sumary}</h4>
-                <p>{post.content}</p>
+    <section className={`${darkMode ? "dark" : "light"}`}>
+      <Navbar dark={darkMode} />
+      <div className="text-right p-6">
+        {data.posts.map(
+          (post) =>
+            post.id === postId && (
+              <div key={post.id}>
+                <p>{post.date}</p>
+                <h2>{post.title}</h2>
+                <div>
+                  <h4>{post.sumary}</h4>
+                  <p>{post.content}</p>
+                </div>
               </div>
-            </div>
-          )
-      )}
+            )
+        )}
+      </div>
     </section>
   );
 }
